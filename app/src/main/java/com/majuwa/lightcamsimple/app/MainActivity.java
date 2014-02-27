2955bfd7dc1b60b10b52d42c9d29c74c707b6387
majuwa <maximilian.j.walter@gmail.com>
2014-02-27 18:35:39 +0100

    Bugs with onResume removed

--- app/src/main/java/com/majuwa/lightcamsimple/app/MainActivity.java
+++ app/src/main/java/com/majuwa/lightcamsimple/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.majuwa.lightcamsimple.app;
 
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.support.v7.app.ActionBarActivity;
@@ -10,6 +11,8 @@ import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.IOException;
 import java.util.List;
@@ -22,10 +25,17 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.initialzie();
+    }
+    public void initialzie(){
+        Button test = (Button)findViewById(R.id.button);
+        test.setText("ON");
+        test.setBackgroundColor(Color.GREEN);
         SurfaceView preview = (SurfaceView) findViewById(R.id.PREVIEW);
+        cam = Camera.open();
         this.holder = preview.getHolder();
         this.holder.addCallback(this);
-        cam = Camera.open();
+        statusLight=false;
         try{
             cam.setPreviewDisplay(this.holder);
         }
@@ -39,6 +49,7 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
     public void surfaceCreated(SurfaceHolder holder) {
         this.holder = holder;
         try{
+            if(cam!=null)
             cam.setPreviewDisplay(holder);
         }
         catch (IOException e){
@@ -57,30 +68,41 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
             cam.setParameters(p);
             cam.stopPreview();
             statusLight = false;
+            Button test = (Button)findViewById(R.id.button);
+            test.setText("ON");
+            test.setBackgroundColor(Color.GREEN);
         }
         else{
             Parameters p = cam.getParameters();
-            Log.v("test",p.getFlashMode());
             List<String> list = p.getSupportedFlashModes();
 
             if(list.contains(Camera.Parameters.FLASH_MODE_TORCH)){
-                Log.v("test","Kamera");
                 p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
 
                 cam.setParameters(p);
                 cam.startPreview();
-                Log.v("test",cam.toString());
                 cam.startPreview();
                 statusLight = true;
+                Button test = (Button)findViewById(R.id.button);
+                test.setText("OFF");
+                test.setBackgroundColor(Color.RED);
             }
 
         }
     }
     @Override
+    public void onResume() {
+        super.onResume();
+        if(cam==null){
+            this.initialzie();
+        }
+    }
+        @Override
     public void onStop(){
         super.onStop();
         if(cam!=null){
             cam.release();
+            cam = null;
         }
     }
     @Override
